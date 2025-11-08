916259306791ffca8f10d6c72120ba6eab42d57e
Naftali Holland <naftaliholland01@gmail.com>
2025-11-08 11:43:41 +0300

    feat: decorator that manages datbase transactions by automatically commiting or rolling back changes

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,42 @@
+import functools
+import sqlite3
+
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = kwargs.get("conn")
+        if conn is None:
+            raise ValueError("connection has to be passed")
+        try:
+            with conn:
+                return func(*args, **kwargs)
+
+        except Exception as e:
+            print(f"Transaction failed: {e}")
+            raise e
+
+    return wrapper
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            conn = sqlite3.connect('users.db')
+            #print("Connection opened")
+            kwargs["conn"] = conn
+            return func(*args, **kwargs)
+
+        finally:
+            conn.close()
+            #print("Connection closed")
+
+    return wrapper
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
