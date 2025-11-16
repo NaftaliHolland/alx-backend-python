10577bb91028ff46fb11a663f6ede76222fa039c
Naftali Holland <naftaliholland01@gmail.com>
2025-11-16 22:20:22 +0300

    fix: add some missing classes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
 from django.http import response
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ class UserViewSet(viewsets.ViewSet):
     """
     User ViewSet
     """
+
     permission_classes = [AllowAny]
 
     def create(self, request):
@@ -23,18 +25,21 @@ class UserViewSet(viewsets.ViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class ConversationViewSet(viewsets.ViewSet):
     """
     A viewset for listing conversations
     """
+
     permission_classes = [AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['conversation_id']
 
     def list(self, request):
         queryset = Conversation.objects.all()
         serializer = ConversationSerializer(queryset, many=True)
 
         return Response(serializer.data)
-
 
     def create(self, request):
         serializer = ConversationSerializer(data=request.data)
@@ -45,24 +50,27 @@ class ConversationViewSet(viewsets.ViewSet):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class MessageViewSet(viewsets.ViewSet):
     """
     A viewset for listing messages
     """
+
     permission_classes = [AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['message_id', 'sender_id', 'message_body']
 
     def list(self, request):
         queryset = Message.objects.all()
         serializer = MessageSerializer(queryset, many=True)
 
         return Response(serializer.data)
-    
+
     def create(self, request):
         serializer = MessageSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
